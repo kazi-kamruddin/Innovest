430a389113b97d70adb47626312a63ee13f89de0
kazi-kamruddin <kazikamruddinahmed@gmail.com>
2025-02-02 03:38:39 +0600

    gonna pull

---
+++ frontEnd/src/pages/pitchInfo.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function PitchInfo() {
+
+  return (
+    <h1 className="pitchInfo">
+          the pitchInfo page.
+    </h1>
+  )
+}
+
+export default PitchInfo;
---
+++ frontEnd/src/pages/signUp.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function SignUp() {
+
+  return (
+    <h1 className="signUp-page">
+          the signUp page.
+    </h1>
+  )
+}
+
+export default SignUp;
