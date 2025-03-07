2da744a0fe882d99b3e42c42a90dbbfe17f05aec
kazi-kamruddin <kazikamruddinahmed@gmail.com>
2025-03-07 18:45:18 +0600

    Fixes #27: now users can register as investor

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -11,6 +11,7 @@ import InvestorProfile from "./pages/InvestorProfile";
 import Allpitches from "./pages/all_pitches";
 import MoreAllPitches from './pages/more_all_pitches'; 
 import AboutUs from "./pages/aboutUs";
+import InvestorInfoSubmit from "./pages/investorInfoSubmit.jsx";
 
 
 import Navbar from "./components/Navbar";  
@@ -38,6 +39,7 @@ function App() {
           {/* <Route path="/dashboard" element={<Dashboard />} /> */}
           <Route path="/dashboard" element={user ? <Dashboard /> : <Navigate to={"/login"} />} />
           <Route path="/investor-profile" element={<InvestorProfile />} /> 
+          <Route path="/investor-info-submit" element={<InvestorInfoSubmit />} />
           <Route path="/editProfile" element={<EditProfile />} /> 
           <Route path="/all-pitches" element={<Allpitches />} />
           <Route path="/all-pitches/:id" element={<MoreAllPitches />} /> 
